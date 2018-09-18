e74071e6f344bf38dd088796e7e750d98f1d8bed
Nick <nickhtpappas@gmail.com>
2018-09-18 15:02:22 -0700

    Had to fix the start script and host port

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.use("/", index);
 app.use("/auth", auth);
 app.use("/ideas", ideas);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
